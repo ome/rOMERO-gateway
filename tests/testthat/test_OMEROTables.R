4b3a8c6249ec6d5477d9c91fb1baad15c2129444
dominikl <d.lindner@dundee.ac.uk>
2017-04-12 13:44:41 +0100

    Added more tests and delete methods

---
+++ tests/testthat/test_OMEROTables.R
@@ -0,0 +1,46 @@
+setup <- read.csv("setup.csv", header = TRUE)
+
+host <- as.character(setup[grep("omero.host", setup$Key, ignore.case=T), ]$Value)
+port <- strtoi(setup[grep("omero.port", setup$Key, ignore.case=T), ]$Value)
+user <- as.character(setup[grep("omero.user", setup$Key, ignore.case=T), ]$Value)
+pass <- as.character(setup[grep("omero.pass", setup$Key, ignore.case=T), ]$Value)
+imageID <- strtoi(setup[grep("imageid", setup$Key, ignore.case=T), ]$Value)
+
+server <- OMEROServer(host=host, port=port, username=user, password=pass)
+server <- connect(server)
+image <- loadObject(server, "ImageData", imageID)
+available <- NULL
+
+# Load "Iris" example dataset
+library(datasets)
+data("iris")
+data.frame(iris)
+
+test_that("Test OMERO attachDataframe",{
+  image <- attachDataframe(image, iris, name='Iris dataset')
+  expect_that(image@dataobject$getId(), equals(imageID))
+})
+
+test_that("Test OMERO availableDataframes",{
+  image <- loadObject(server, "ImageData", imageID)
+  available <<- availableDataframes(image)
+  expect_that(nrow(available), equals(1))
+})
+
+test_that("Test OMERO loadDataframe",{
+  df <- loadDataframe(image, available$ID[1])
+  expect_that(nrow(df), equals(nrow(iris)))
+})
+
+test_that("Test OMERO describeDataframe",{
+  info <- describeDataframe(image, available$ID[1])
+  expect_that(nrow(info), equals(5))
+})
+
+test_that("Test OMERO deleteFile",{
+  deleteFile(image, available$AnnotationID[1])
+  available <- availableDataframes(image)
+  expect_that(nrow(available), equals(0))
+})
+
+disconnect(server)
