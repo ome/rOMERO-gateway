b9d23f49e80a2e434b2a0e1d2dbb760c15c2896e
dominikl <d.lindner@dundee.ac.uk>
2017-05-18 08:53:16 +0100

    Load wells via getWells() method

--- tests/testthat/test-OMEROServer.R
+++ tests/testthat/test-OMEROServer.R
@@ -9,6 +9,10 @@ imageID <- strtoi(setup[grep("imageid", setup$Key, ignore.case=T), ]$Value)
 nAnnos <- strtoi(setup[grep("numberofannotations", setup$Key, ignore.case=T), ]$Value)
 
 screenID <- strtoi(setup[grep("screenid", setup$Key, ignore.case=T), ]$Value)
+plateID <- strtoi(setup[grep("plateid", setup$Key, ignore.case=T), ]$Value)
+projectID <- strtoi(setup[grep("projectid", setup$Key, ignore.case=T), ]$Value)
+datasetID <- strtoi(setup[grep("datasetid", setup$Key, ignore.case=T), ]$Value)
+
 mapannotation <- as.character(setup[grep("mapannotation", setup$Key, ignore.case=T), ]$Value)
   
 server <- NULL
@@ -20,12 +24,44 @@ test_that("Test OMEROServer connect",{
   expect_that(s@user$getId(), is_a("numeric"))
 })
 
-test_that("Test OMEROServer loadObject",{
+test_that("Test OMEROServer loadObject Image",{
   image <- loadObject(server, "ImageData", imageID)
   expect_that(image@dataobject$getId(), equals(imageID))
   
   clazz <- class(image)[[1]]
   expect_that(clazz, equals('Image'))
+})
+
+test_that("Test OMEROServer loadObject Screen",{
+  screen <- loadObject(server, "ScreenData", screenID)
+  expect_that(screen@dataobject$getId(), equals(screenID))
+  
+  clazz <- class(screen)[[1]]
+  expect_that(clazz, equals('Screen'))
+})
+
+test_that("Test OMEROServer loadObject Plate",{
+  plate <- loadObject(server, "PlateData", plateID)
+  expect_that(plate@dataobject$getId(), equals(plateID))
+  
+  clazz <- class(plate)[[1]]
+  expect_that(clazz, equals('Plate'))
+})
+
+test_that("Test OMEROServer loadObject Project",{
+  proj <- loadObject(server, "ProjectData", projectID)
+  expect_that(proj@dataobject$getId(), equals(projectID))
+  
+  clazz <- class(proj)[[1]]
+  expect_that(clazz, equals('Project'))
+})
+
+test_that("Test OMEROServer loadObject Dataset",{
+  ds <- loadObject(server, "DatasetData", datasetID)
+  expect_that(ds@dataobject$getId(), equals(datasetID))
+  
+  clazz <- class(ds)[[1]]
+  expect_that(clazz, equals('Dataset'))
 })
 
 test_that("Test OMEROServer getAnnotations",{
